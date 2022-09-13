1618ccd19c15c4b06f198d95f1d1ac4a5308c2f3
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-09-13 21:20:40 +0700

    style: change arbitary value for border radius to tailwind class

--- src/components/organisms/Sidebar/HelpCenter.tsx
+++ src/components/organisms/Sidebar/HelpCenter.tsx
@@ -1,12 +1,12 @@
 const HelpCenter = () => {
   return (
     <div className="relative">
-      <div className="w-full h-full bg-secondary-500 text-primary-0 hover:bg-primary-500 peer pointer-events-none rounded-[10px] relative overflow-hidden px-4 pb-4 text-center transition-all duration-500">
+      <div className="w-full h-full bg-secondary-500 text-primary-0 hover:bg-primary-500 peer pointer-events-none rounded-default relative overflow-hidden px-4 pb-4 text-center transition-all duration-500">
         <p className="font-semibold text-base mt-[71px]">Help Center</p>
         <p className="font-medium text-xs mt-3">
           Having Trouble in Learning. Please contact us for more questions.
         </p>
-        <button className="bg-primary-0 w-full py-3 rounded-[10px] mt-[45px] pointer-events-auto text-secondary-500 hover:text-primary-500 peer text-xs font-semibold relative cursor-pointer z-20 transition-all duration-500">
+        <button className="bg-primary-0 w-full py-3 rounded-default mt-[45px] pointer-events-auto text-secondary-500 hover:text-primary-500 peer text-xs font-semibold relative cursor-pointer z-20 transition-all duration-500">
           Go To Help Center
         </button>
         <div className="w-40 h-40 bg-[#262733] peer-hover:bg-primary-300 rounded-[50%] absolute -top-[100px] -left-[94px] transition-all duration-500"></div>
