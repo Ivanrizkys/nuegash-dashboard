88eb0af4f623d11376d11d3d397b24b046d2f301
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-09-21 21:03:19 +0700

    feat: adding search icon

---
+++ src/assets/icons/Search.tsx
@@ -0,0 +1,28 @@
+const Search = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="none"
+      viewBox="0 0 20 20"
+    >
+      <g
+        stroke="#8E92BC"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        clipPath="url(#clip0_372_4950)"
+      >
+        <path d="M9.584 17.5a7.917 7.917 0 100-15.833 7.917 7.917 0 000 15.833zM18.334 18.333l-1.667-1.666"></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_372_4950">
+          <path fill="#fff" d="M0 0H20V20H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default Search;
