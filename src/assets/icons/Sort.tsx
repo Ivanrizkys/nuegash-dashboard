41b320fcc5aea5dc2051912bd7bfb98281e4e904
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-09-26 13:17:25 +0700

    feat: adding new icons: category filter and sort by filter

---
+++ src/assets/icons/Sort.tsx
@@ -0,0 +1,20 @@
+const Sort = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#8E92BC"
+        strokeLinecap="round"
+        strokeWidth="1.5"
+        d="M3 7h18M6 12h12M10 17h4"
+      ></path>
+    </svg>
+  );
+};
+
+export default Sort
