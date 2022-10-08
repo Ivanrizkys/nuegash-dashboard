94f3867e75ce103d5cc43f170ad6576f616bf509
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-10-08 20:19:00 +0700

    fix: custom css for styling swiper container affect other component

--- src/components/organisms/MentorSlide/index.tsx
+++ src/components/organisms/MentorSlide/index.tsx
@@ -1,9 +1,11 @@
+import "swiper/css";
 import { useId } from "react";
 import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import ArrowLeft from "@/src/assets/icons/ArrowLeft";
 import ArrowRight from "@/src/assets/icons/ArrowRight";
 import CardMentor from "@/src/components/molecules/CardMentor";
+import { SwiperContainer } from "@/src/styles/index";
 
 interface MentorSlideProps {
   title: string;
@@ -35,30 +37,32 @@ const MentorSlide = ({ title, swiperClass }: MentorSlideProps) => {
         </div>
       </div>
       <div className="mt-[20px]">
-        <Swiper
-          navigation={{
-            nextEl: `.${swiperClass}-next`,
-            prevEl: `.${swiperClass}-prev`,
-          }}
-          modules={[Navigation]}
-          className={swiperClass}
-          slidesPerView={"auto"}
-          spaceBetween={32}
-        >
-          {[0, 1, 2, 3, 4, 5].map((value) => (
-            <SwiperSlide key={useId()}>
-              <CardMentor
-                name="Cika Febriana"
-                role="UI UX Design"
-                task={40}
-                rating={4.7}
-                review={750}
-                avatar="https://bit.ly/3QEIVsR"
-                isFollowed={false}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <SwiperContainer>
+          <Swiper
+            navigation={{
+              nextEl: `.${swiperClass}-next`,
+              prevEl: `.${swiperClass}-prev`,
+            }}
+            modules={[Navigation]}
+            className={swiperClass}
+            slidesPerView={"auto"}
+            spaceBetween={32}
+          >
+            {[0, 1, 2, 3, 4, 5].map((value) => (
+              <SwiperSlide key={useId()}>
+                <CardMentor
+                  name="Natasya Sifa Aulia"
+                  role="UI UX Design"
+                  task={40}
+                  rating={4.7}
+                  review={750}
+                  avatar="https://s3-alpha-sig.figma.com/img/fcca/fd50/f3b563f82df0cdeb796dc899e0e48a48?Expires=1665964800&Signature=hEJi0bOPpIJJ8IPpAaa5XCpmcHsaOfbyw9xz-pOoFRuABOB5IvJENyYcqhcHBtNhogV1voG2jpOCKcCkv0h91TBAhbSkMA6iwZGuNj~fpzYoKCAicnx0L7n5q7WKSPIGoh1L3bkZZtZqIwTPCTwH6nj-MkrIs~V4AmsOByKpgK9XlpauB8HIuYviEHeQOJq8oFA4j63AAi4yMFAkVkA41i0~mQ-tT0Up6Y9MNsRdZcQ-9L0-GiymCvg2CQQlWxvFvwcu2~3RKDc2zCG9v3BUln8tZmg-5YNacUCdRMfsIm6cHUf0PpYlrenijM8Ha0yOgYjh~ilWzPDgLxbGzcigEQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  isFollowed={false}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </SwiperContainer>
       </div>
     </div>
   );
--- src/components/organisms/TaskSlide/index.tsx
+++ src/components/organisms/TaskSlide/index.tsx
@@ -1,9 +1,11 @@
+import "swiper/css";
 import { useId } from "react";
 import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import ArrowLeft from "@/src/assets/icons/ArrowLeft";
 import ArrowRight from "@/src/assets/icons/ArrowRight";
 import CardTask from "@/src/components/molecules/CardTask";
+import { SwiperContainer } from "@/src/styles/index";
 
 interface TaskSlideProps {
   title: string;
@@ -37,39 +39,41 @@ const TaskSlide = ({ title, swiperClass }: TaskSlideProps) => {
         </div>
       </div>
       <div className="mt-[20px]">
-        <Swiper
-          navigation={{
-            nextEl: `.${swiperClass}-next`,
-            prevEl: `.${swiperClass}-prev`,
-          }}
-          modules={[Navigation]}
-          className={swiperClass}
-          slidesPerView={"auto"}
-          spaceBetween={32}
-        >
-          {[0, 1, 2, 3, 4, 5].map((value) => (
-            <SwiperSlide key={useId()}>
-              <CardTask
-                image="https://bit.ly/3L8H2Ds"
-                title="Creating Mobile App Design"
-                role="UI UX Design"
-                progress={15}
-                timeRemaining="3 Days Left"
-                contributor={[
-                  {
-                    image: "https://bit.ly/3QEIVsR",
-                  },
-                  {
-                    image: "https://bit.ly/3QEIVsR",
-                  },
-                  {
-                    image: "https://bit.ly/3QEIVsR",
-                  },
-                ]}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <SwiperContainer>
+          <Swiper
+            navigation={{
+              nextEl: `.${swiperClass}-next`,
+              prevEl: `.${swiperClass}-prev`,
+            }}
+            modules={[Navigation]}
+            className={swiperClass}
+            slidesPerView={"auto"}
+            spaceBetween={32}
+          >
+            {[0, 1, 2, 3, 4, 5].map((value) => (
+              <SwiperSlide key={useId()}>
+                <CardTask
+                  image="https://s3-alpha-sig.figma.com/img/3383/da9a/5e6800bc07f249dba70a9128599ffaa6?Expires=1665964800&Signature=cis4e-5Vk6qcNR70H4sJkoAUogsFzeoOxnaX~cYs9-0rrDjBsh4jPeqLiUWe-fQgFX5XIut3r523FwvQRcVP8rGbOrabMHDsSXkG2UGSNT2~yXdDSsKlIf5ADPd9ZViIuBfn4ZBXhRobECaaLYTdY6me29pIFa-vjo38ANnZcdN620m9sZL10JX2yZ65zA8pmDwZ8mSmPhsVoh6bFONAOdJxMM6ly~jXts0sl~vTuzP-Iamt4NGgDb4O0ypInOZajMbPUC0FL6dqvJLOi4Fvn-4zDr5aXy1N-us9Gw28op4uHe~SOeaHqIMGWwoYPszOEQKAWiTOK8BSzUZvXYqSdw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  title="Creating Mobile App Design"
+                  role="UI UX Design"
+                  progress={15}
+                  timeRemaining="3 Days Left"
+                  contributor={[
+                    {
+                      image: "https://s3-alpha-sig.figma.com/img/375b/7a9f/c6fd3f7edcc62885c94d899223b36b74?Expires=1665964800&Signature=MnnXB4ropFHUfqzfdAhNs9Q95rdgamLgBsoi4gn9-9ET5oTP8hwQ94ylrVOO0ILJQe4DfpV5cpJmTgQmnoU4Pxqu8KTVJFgyo5BHkxsLatVN1r2~CxWwtRhC~-~ty8nuqz~QfkkyTrhB~chuuB4HwL-pDPbv-kc~JfqAbQ8zoyZQqYfe~pcem2FPDbHF5iwclbHQzuV~Ulj0E3MWLBJujhN2erlr26JvdWs~C1Hn4C42apIGMEUN~1qD46MmBs0KnK1tspWS6gkwCPODSN-Z6ZUNfvoDuj5lVTBM026KtKXfsfY~B~mtEsFv7Q6pQ80iO1tLcGCELIUKbW34jQ3PDQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+                    },
+                    {
+                      image: "https://s3-alpha-sig.figma.com/img/375b/7a9f/c6fd3f7edcc62885c94d899223b36b74?Expires=1665964800&Signature=MnnXB4ropFHUfqzfdAhNs9Q95rdgamLgBsoi4gn9-9ET5oTP8hwQ94ylrVOO0ILJQe4DfpV5cpJmTgQmnoU4Pxqu8KTVJFgyo5BHkxsLatVN1r2~CxWwtRhC~-~ty8nuqz~QfkkyTrhB~chuuB4HwL-pDPbv-kc~JfqAbQ8zoyZQqYfe~pcem2FPDbHF5iwclbHQzuV~Ulj0E3MWLBJujhN2erlr26JvdWs~C1Hn4C42apIGMEUN~1qD46MmBs0KnK1tspWS6gkwCPODSN-Z6ZUNfvoDuj5lVTBM026KtKXfsfY~B~mtEsFv7Q6pQ80iO1tLcGCELIUKbW34jQ3PDQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+                    },
+                    {
+                      image: "https://s3-alpha-sig.figma.com/img/375b/7a9f/c6fd3f7edcc62885c94d899223b36b74?Expires=1665964800&Signature=MnnXB4ropFHUfqzfdAhNs9Q95rdgamLgBsoi4gn9-9ET5oTP8hwQ94ylrVOO0ILJQe4DfpV5cpJmTgQmnoU4Pxqu8KTVJFgyo5BHkxsLatVN1r2~CxWwtRhC~-~ty8nuqz~QfkkyTrhB~chuuB4HwL-pDPbv-kc~JfqAbQ8zoyZQqYfe~pcem2FPDbHF5iwclbHQzuV~Ulj0E3MWLBJujhN2erlr26JvdWs~C1Hn4C42apIGMEUN~1qD46MmBs0KnK1tspWS6gkwCPODSN-Z6ZUNfvoDuj5lVTBM026KtKXfsfY~B~mtEsFv7Q6pQ80iO1tLcGCELIUKbW34jQ3PDQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+                    },
+                  ]}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </SwiperContainer>
       </div>
     </div>
   );
