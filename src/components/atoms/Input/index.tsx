d1ac608e49701fb89ba46510656e09b778bd62b5
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-10-08 20:24:48 +0700

    feat: input component now can recieve ref that declare on instance components

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { forwardRef } from "react"
 
 interface InputProps {
   placeholder: string
@@ -7,16 +7,16 @@ interface InputProps {
   adormentPosition?: "start" | "end"
 }
 
-const Input = (props: InputProps) => {
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   const { placeholder, type, inputAdorment, adormentPosition } = props
   return (
     <div className={`w-full inline-flex items-center gap-x-7 border-solid border-[1px] border-[#F5F5F7] rounded-default ${adormentPosition === "start" ? "flex-row" : "flex-row-reverse"} py-[14px] px-7`}>
       <div className="flex items-center justify-center">
         {inputAdorment}
       </div>
-      <input type="text" placeholder={placeholder} className="focus:outline-none w-full text-xs text-secondary-400" />
+      <input ref={ref} type={type} placeholder={placeholder} className="focus:outline-none w-full text-xs text-secondary-400" />
     </div>
   )
-}
+})
 
 export default Input
