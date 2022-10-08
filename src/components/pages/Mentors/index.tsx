0872b4664559d8a3780798a37a535e4eba764880
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-10-08 20:20:45 +0700

    fix: cannot load image

--- src/components/pages/Mentors/index.tsx
+++ src/components/pages/Mentors/index.tsx
@@ -1,4 +1,3 @@
-import { useId } from "react"
 import AppBar from "@/src/components/organisms/AppBar"
 import MentorSlide from "@/src/components/organisms/MentorSlide";
 import CardMentor from "@/src/components/molecules/CardMentor"
@@ -32,7 +31,7 @@ const Task = () => {
                 task={40}
                 rating={4.7}
                 review={750}
-                avatar="https://bit.ly/3QEIVsR"
+                avatar="https://s3-alpha-sig.figma.com/img/375b/7a9f/c6fd3f7edcc62885c94d899223b36b74?Expires=1665964800&Signature=MnnXB4ropFHUfqzfdAhNs9Q95rdgamLgBsoi4gn9-9ET5oTP8hwQ94ylrVOO0ILJQe4DfpV5cpJmTgQmnoU4Pxqu8KTVJFgyo5BHkxsLatVN1r2~CxWwtRhC~-~ty8nuqz~QfkkyTrhB~chuuB4HwL-pDPbv-kc~JfqAbQ8zoyZQqYfe~pcem2FPDbHF5iwclbHQzuV~Ulj0E3MWLBJujhN2erlr26JvdWs~C1Hn4C42apIGMEUN~1qD46MmBs0KnK1tspWS6gkwCPODSN-Z6ZUNfvoDuj5lVTBM026KtKXfsfY~B~mtEsFv7Q6pQ80iO1tLcGCELIUKbW34jQ3PDQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
                 isFollowed={false}
                 description="Hi, I'm Cika Febriana Putri. I'm an Android Developer at Google company . . ."
               />
