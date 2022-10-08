6ea2d6175d636ff64c88586957198c1ca5544e81
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-10-08 20:11:57 +0700

    feat: implement coming soon ui on messages

---
+++ src/components/pages/Messages/index.tsx
@@ -0,0 +1,9 @@
+const Messages = () => {
+  return (
+    <div className="xl:ml-[252px] min-h-screen flex justify-center items-center bg-[#FAFAFA]">
+      <p className="text-4xl font-semibold text-secondary-500">Coming Soon :)</p>
+    </div>
+  )
+}
+
+export default Messages
