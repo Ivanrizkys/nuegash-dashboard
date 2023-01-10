da9e678525d5b27a1844444743b0d45366805309
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-01-10 21:48:23 +0700

    feat: hide sidebar on auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Routes from "@/src/routes/index";
+import { useLocation } from "react-router-dom";
 import Spinner from "@/src/components/atoms/Spinner";
 import Sidebar from "@/src/components/organisms/Sidebar";
 
 function App() {
+  const { pathname } = useLocation() 
+
   return (
     <div className="font-sans box-border">
-      <Sidebar />
+      {!pathname.includes("auth") && (
+        <Sidebar />
+      )}
       <React.Suspense fallback={<Spinner />}>
         <Routes />
       </React.Suspense>
