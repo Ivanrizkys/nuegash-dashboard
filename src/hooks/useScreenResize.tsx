da1db6430f4e6a5f2d4bb1cb6993a77d2793e0a2
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-03-23 12:34:33 +0700

    feat: change behavior hooks 'only allow this hooks runing in initial render'

--- src/hooks/useScreenResize.tsx
+++ src/hooks/useScreenResize.tsx
@@ -4,25 +4,25 @@ import { updateScreenWidth, updateShowSidebar } from "../global/app";
 
 const useScreenResize = () => {
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
-    dispatch(updateScreenWidth(window.innerWidth))
+    dispatch(updateScreenWidth(window.innerWidth));
     if (window.innerWidth >= 1280) {
-      dispatch(updateShowSidebar(true))
+      dispatch(updateShowSidebar(true));
     } else {
-      dispatch(updateShowSidebar(false))
+      dispatch(updateShowSidebar(false));
     }
     const handleResizeScreen = () => {
       if (window.innerWidth >= 1280) {
-        dispatch(updateShowSidebar(true))
+        dispatch(updateShowSidebar(true));
       } else {
-        dispatch(updateShowSidebar(false))
+        dispatch(updateShowSidebar(false));
       }
-      dispatch(updateScreenWidth(window.innerWidth))
+      dispatch(updateScreenWidth(window.innerWidth));
     };
     window.addEventListener("resize", handleResizeScreen);
     return () => window.removeEventListener("resize", handleResizeScreen);
-  });
+  }, []);
 };
 
 export default useScreenResize;
