260f89fd6bc17aab16d0f5e5057eebda5d47490b
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-03-27 21:38:50 +0700

    fix: ugly ui in tablet and mobile mode

--- src/components/molecules/UserAction/index.tsx
+++ src/components/molecules/UserAction/index.tsx
@@ -1,4 +1,6 @@
 import Notification from "@/src/assets/icons/Notification";
+import { RootState } from "@/src/global/store";
+import { useSelector } from "react-redux";
 
 interface UserActionProps {
   userImg: string;
@@ -6,14 +8,19 @@ interface UserActionProps {
 }
 
 const UserAction = ({ userImg, notificationActive }: UserActionProps) => {
+  const screenWidth = useSelector((state: RootState) => state.app.screenWidth);
+
   return (
     <div className="flex items-center gap-x-6">
       <div className="cursor-pointer">
-        <Notification active={notificationActive} />
+        <Notification
+          active={notificationActive}
+          variant={screenWidth > 640 ? "large" : "small"}
+        />
       </div>
       <img
         src={userImg}
-        className="w-[52px] h-[52px] cursor-pointer"
+        className="w-[44px] h-[44px] sm:w-[52px] sm:h-[52px] cursor-pointer"
         alt="profile"
       />
     </div>
