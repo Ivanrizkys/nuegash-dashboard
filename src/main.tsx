b40d31a03af84a65edeb64f27e987196ed3e2b85
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-09-07 09:06:07 +0700

    feat: installing apolo provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import "./index.css";
 import React from "react";
 import App from "./App";
+import client from "./config/client";
 import ReactDOM from "react-dom/client";
 import { store } from "@/src/global/store";
+import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ReduxProvider store={store}>
-        <App />
-      </ReduxProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <ReduxProvider store={store}>
+          <App />
+        </ReduxProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
