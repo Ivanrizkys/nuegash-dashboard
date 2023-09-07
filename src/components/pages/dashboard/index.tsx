29a1fd709278cdbdd0a16f93c02f8536c64dc6cf
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-09-07 09:20:24 +0700

    feat: consume data from supabase using graphql

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -1,115 +1,37 @@
 import "./custom.css";
 import "swiper/css/navigation";
 import "react-circular-progressbar/dist/styles.css";
-import ArrowDown from "@/src/assets/icons/ArrowDown";
 import AppBar from "@/src/components/organisms/AppBar";
 import TaskSlide from "@/src/components/organisms/TaskSlide";
 import TaskToday from "@/src/components/organisms/TaskToday";
+import RunningTask from "@/src/components/molecules/RunningTask"
+import Activity from "@/src/components/molecules/Activity"
 import MentorSlide from "@/src/components/organisms/MentorSlide";
-import { CircularProgressbar } from "react-circular-progressbar";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  ChartOptions,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
+import { useQuery } from "@apollo/client";
+import { GET_OVERVIEW_DATA } from "@/src/service/query";
+import { useMemo } from 'react';
+import { ActivityData, RunningTaskData } from "@/src/libs/dto/json";
+
 
 const Dashboard = () => {
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-
-  const options: ChartOptions<any> = {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      y: {
-        border: {
-          display: false,
-        },
-        grid: {
-          display: false,
-        },
-        ticks: {
-          color: "#141522",
-          font: {
-            size: 12,
-            weight: "600",
-          },
-          callback: (value: number) => {
-            return value;
-          },
-        },
-      },
-      x: {
-        border: {
-          display: false,
-        },
-        grid: {
-          color: "#F5F5F7",
-        },
-        ticks: {
-          font: {
-            size: 12,
-            weight: "600",
-          },
-          color: "#141522",
-        },
-      },
-    },
-    elements: {
-      line: {
-        tension: 0.5,
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-        position: "top" as const,
-      },
-      tooltip: {
-        displayColors: false,
-        bodyFont: {
-          size: 14,
-          weight: "600",
-        },
-        padding: {
-          x: 18,
-          y: 8,
-        },
-        callbacks: {
-          title: () => null,
-          label: ({ raw }: { raw: number }) => `${raw} Task`,
-        },
-      },
-    },
-  };
-
-  const labels = ["S", "M", "T", "W", "T", "F", "S"];
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "Task",
-        data: [1, 2, 1, 3, 2, 2.3, 2.8],
-        borderColor: "#141522",
-        backgroundColor: "#FFFF",
-        color: "#141522",
-      },
-    ],
-  };
+  const { data } = useQuery(GET_OVERVIEW_DATA)
+  
+  const activity = useMemo<ActivityData>(() => {
+    const temp: ActivityData = {
+      labels: ["S", "M", "T", "W", "T", "F", "S"],
+      data: [0, 0, 0, 0, 0, 0, 0]
+    }
+    return data ? JSON.parse(data?.activity?.edges[0]?.node?.data) : temp;
+  }, [data])
+  
+  const runningTask = useMemo<RunningTaskData>(() => {
+    const temp: RunningTaskData = {
+      onProgress: 0,
+      percentage: 0,
+      total: 0
+    }
+    return data ? JSON.parse(data?.runningTask?.edges[0]?.node?.data) : temp;
+  }, [data])
 
   return (
     <div className="xl:ml-[252px] flex flex-col lg:flex-row bg-[#FAFAFA] min-h-screen">
@@ -129,92 +51,79 @@ const Dashboard = () => {
             Let's finish your task today
           </h3>
         </div>
+
         <div className="flex justify-between flex-col sm:flex-row gap-8 mt-11">
-          <div className="w-full md:min-w-[194px] md:w-2/12 bg-secondary-500 rounded-default p-[20px] text-primary-0 flex items-center justify-between min-[370px]:justify-around sm:flex-col sm:justify-start sm:items-start">
-            <div className="font-semibold">
-              <p className="text-base">Running Task</p>
-              <p className="text-[32px] my-4 text-center sm:text-left">65</p>
-            </div>
-            <div className="flex items-center gap-x-[18px]">
-              <div className="w-[99px] h-[99px] sm:w-[68px] sm:h-[68px]">
-                <CircularProgressbar
-                  value={45}
-                  text={`${45}%`}
-                  styles={{
-                    path: {
-                      stroke: "#546FFF",
-                      strokeWidth: "3px",
-                    },
-                    trail: {
-                      stroke: "#1A1E38",
-                      strokeWidth: "3px",
-                    },
-                    text: {
-                      fill: "#FFFFFF",
-                      fontSize: "18px",
-                      fontWeight: "500",
-                    },
-                  }}
-                />
-              </div>
-              <div>
-                <p className="text-xl font-semibold">100</p>
-                <p className="text-sm font-medium">Task</p>
-              </div>
-            </div>
-          </div>
-          <div className="w-full md:w-10/12 bg-[#F5F5F7] rounded-default p-5">
-            <div className="flex items-center justify-between text-secondary-500">
-              <p className="font-semibold text-base">Activity</p>
-              <div className="flex items-center gap-x-2">
-                <p className="font-medium text-xs">This Week</p>
-                <div className="cursor-pointer">
-                  <ArrowDown variant="small" />
-                </div>
-              </div>
-            </div>
-            <div className="flex bg-white p-4 rounded-default mt-5 w-full">
-              <div className="h-[98px] w-full relative">
-                <Line options={options} data={data} />
-              </div>
-            </div>
-          </div>
+          {data?.runningTask &&
+            <RunningTask 
+              total={runningTask.total}
+              percentage={runningTask.percentage}
+              onProgress={runningTask.onProgress}
+            />
+          }
+          {data?.activity &&
+            <Activity 
+              data={activity.data}
+              labels={activity.labels}
+            />
+          }
         </div>
 
         <section className="mt-8">
+        {data?.mentorsCollection && 
           <MentorSlide
             title="Monthly Mentors"
             swiperClass="monthly-mentors-list"
+            mentors={data?.mentorsCollection?.edges.map((mentor) => ({
+              id: mentor?.node?.id as string,
+              name: mentor?.node?.name ?? "",
+              role: mentor?.node?.role ?? "",
+              avatar: mentor?.node?.image ?? "",
+              rating: mentor?.node?.rating as number,
+              task: mentor?.node?.total_task ?? 0,
+              review: mentor?.node?.total_review ??0,
+              isFollowed: mentor?.node?.is_followed ?? false
+            }))}
           />
+        }
         </section>
 
         <section className="text-secondary-500 mt-8">
-          <TaskSlide title="Upcoming Task" swiperClass="upcoming-task-list" />
+          {data?.tasksCollection &&
+            <TaskSlide 
+              title="Upcoming Task" 
+              swiperClass="upcoming-task-list"
+              tasks={data?.tasksCollection?.edges.map((task) => ({
+                id: task?.node?.id as string,
+                image: task?.node?.image ?? "",
+                role: task?.node?.task_categories?.name ?? "",
+                title: task?.node?.title ?? "",
+                progress: task?.node?.progress ?? 0,
+                timeRemaining: new Date("1995-12-17T03:24:00"),
+                contributors: JSON.parse(task?.node?.mentors).map((image: string) => ({
+                  image
+                }))
+              }))}
+            />
+          }
         </section>
       </div>
+
       <div className="lg:w-4/12 p-6 sm:p-8 bg-[#F5F5F7] flex flex-col sm:flex-row-reverse gap-8 lg:gap lg:flex-col">
         <div className="bg-primary-0 w-full h-[160px] rounded-default"></div>
-        <TaskToday
-          image="https://res.cloudinary.com/draaoe7rc/image/upload/v1672716435/nuegas/task/task-1_t57skg.png"
-          title="Creating Mobile App Design"
-          role="UI UX Design"
-          progress={15}
-          timeRemaining="3 Days Left"
-          contributor={[
-            {
-              image:
-                "https://res.cloudinary.com/draaoe7rc/image/upload/v1672717660/nuegas/mentor/mentor-1_io3lzd.png",
-            },
-            {
-              image:
-                "https://res.cloudinary.com/draaoe7rc/image/upload/v1672717660/nuegas/mentor/mentor-5_oecy53.png",
-            },
-            {
-              image:
-                "https://res.cloudinary.com/draaoe7rc/image/upload/v1672717660/nuegas/mentor/mentor-3_m4cy4u.png",
-            },
-          ]}
-        />
+        
+        {data?.taskToday && 
+          <TaskToday
+            image={data?.taskToday?.edges[0]?.node?.image ?? ""}
+            title={data?.taskToday?.edges[0]?.node?.title ?? ""}
+            role={data?.taskToday?.edges[0]?.node?.task_categories?.name ?? ""}
+            progress={data?.taskToday?.edges[0]?.node?.progress ?? 0}
+            timeRemaining="3 Days Left"
+            contributor={JSON.parse(data?.taskToday?.edges[0]?.node?.mentors).map((image: string) => ({
+              image
+            }))}
+            assessments={JSON.parse(data?.taskToday?.edges[0]?.node?.assessment)}
+          />
+        }
       </div>
     </div>
   );
