0d76c9f957a4e24ce3de5103fa7c83eb4fe98dd5
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-10-07 18:49:50 +0700

    feat: adding skeleton loading in mentors page

--- src/components/pages/Mentors/index.tsx
+++ src/components/pages/Mentors/index.tsx
@@ -1,63 +1,13 @@
-import AppBar from "@/src/components/organisms/AppBar"
-import MentorSlide from "@/src/components/organisms/MentorSlide";
-import CardMentor from "@/src/components/molecules/CardMentor"
-import { useQuery } from "@apollo/client";
-import { GET_MENTORS_DATA } from "@/src/service/query";
+import { Suspense } from "react";
+import MentorsLoader from "./loader";
+import MentorsContent from "./content";
 
-const Task = () => {
-  const { data } = useQuery(GET_MENTORS_DATA)
-  
-
+const Mentors = () => {
   return (
-    <div className="xl:ml-[252px] bg-[#FAFAFA] min-h-screen">
-      <nav className="bg-primary-0 p-8 w-full">
-        <AppBar 
-          title="Explore Mentors"
-          userImg={"/profile.png"}
-          notificationActive={true}
-          withSearch={true}
-          placeholder="Search Mentors"
-        />
-      </nav>
-      <div className="p-8">
-        <section>
-          {data?.recentMentors && 
-            <MentorSlide
-              title="Recent Mentors"
-              swiperClass="recent-mentors-list"
-              mentors={data?.recentMentors?.edges.map((mentor) => ({
-                id: mentor?.node?.id as string,
-                name: mentor?.node?.name ?? "",
-                role: mentor?.node?.role ?? "",
-                avatar: mentor?.node?.image ?? "",
-                rating: mentor?.node?.rating as number,
-                task: mentor?.node?.total_task ?? 0,
-                review: mentor?.node?.total_review ?? 0,
-                isFollowed: mentor?.node?.is_followed ?? false
-              }))}
-            />
-          }
-        </section>
-        <section className="mt-8">
-          <h2 className="text-2xl font-semibold text-secondary-500 mb-[18px]">Mentors</h2>
-          <div className="grid grid-cols-[repeat(auto-fit,_minmax(352px,_1fr))] gap-8">
-            {data?.mentors && data?.mentors?.edges.map((mentor) => (
-              <CardMentor 
-                name={mentor?.node?.name ?? ""}
-                role={mentor?.node?.role ?? ""}
-                task={mentor?.node?.total_task ?? 0}
-                rating={mentor?.node?.rating as number}
-                review={mentor?.node?.total_review ?? 0}
-                avatar={mentor?.node?.image ?? ""}
-                isFollowed={mentor?.node?.is_followed ?? false}
-                description={mentor?.node?.description ?? ""}
-              />
-            ))}
-          </div>
-        </section>
-      </div>
-    </div>
-  )
-}
+    <Suspense fallback={<MentorsLoader />}>
+      <MentorsContent />
+    </Suspense>
+  );
+};
 
-export default Task
+export default Mentors;
---
+++ src/components/pages/Mentors/loader.tsx
@@ -0,0 +1,39 @@
+import AppBar from "@/src/components/organisms/AppBar";
+import CardMentorLoader from "@/src/components/molecules/CardMentor/loader";
+import MentorSlideLoader from "@/src/components/organisms/MentorSlide/loader";
+
+const MentorsLoader = () => {
+  return (
+    <div className="xl:ml-[252px] bg-[#FAFAFA] min-h-screen">
+      <nav className="bg-primary-0 p-8 w-full">
+        <AppBar
+          title="Explore Mentors"
+          userImg={"/profile.png"}
+          notificationActive={true}
+          withSearch={true}
+          placeholder="Search Mentors"
+        />
+      </nav>
+      <div className="p-8">
+        <section>
+          <MentorSlideLoader
+            title="Recent Mentors"
+            swiperClass="recent-mentors-list-loader"
+          />
+        </section>
+        <section className="mt-8">
+          <h2 className="text-2xl font-semibold text-secondary-500 mb-[18px]">
+            Mentors
+          </h2>
+          <div className="grid grid-cols-[repeat(auto-fit,_minmax(352px,_1fr))] gap-8">
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((val) => (
+              <CardMentorLoader key={val} autoWidth withDescription />
+            ))}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default MentorsLoader;
