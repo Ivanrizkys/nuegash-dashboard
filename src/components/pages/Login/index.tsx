cee73b28b9ca6cf2f10e59e2e676c463d1b30ad7
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-10-12 21:22:32 +0700

    feat: implement login authentication

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,8 +1,16 @@
-import { Link } from "react-router-dom";
+import Cookies from "js-cookie"
+import { useState } from "react";
+import toast from "react-hot-toast";
+import useAuth from "@/src/hooks/useAuth";
+import { useDispatch } from "react-redux";
 import Logo from "@/src/assets/icons/Logo";
+import { updateUser } from "@/src/global/user";
 import Input from "@/src/components/atoms/Input";
+import supabaseClient from "@/src/config/supabase";
 import Button from "@/src/components/atoms/Button";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { AuthTokenResponse } from "@supabase/supabase-js";
+import { Link, useNavigate, Navigate } from "react-router-dom";
 
 interface LoginFormValues {
   Email: string;
@@ -10,15 +18,62 @@ interface LoginFormValues {
 }
 
 const Login = () => {
+  const auth = useAuth()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState<boolean>(false);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<LoginFormValues>();
 
-  const handleLogin: SubmitHandler<LoginFormValues> = (data) => {
-    console.log(data);
+  const supabaseLogin = (data: LoginFormValues): Promise<AuthTokenResponse> => {
+    return new Promise((resolve, reject) => {
+      supabaseClient.auth
+        .signInWithPassword({
+          email: data.Email,
+          password: data.Password,
+        })
+        .then((res) => {
+          if (res.error) {
+            reject(res.error);
+          }
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
   };
+
+  const handleLogin: SubmitHandler<LoginFormValues> = async (data) => {
+    setLoading(true);
+    toast.promise<AuthTokenResponse>(supabaseLogin(data), {
+      loading: "Signing in...",
+      success: (res) => {        
+        Cookies.set("token", res.data.session?.access_token as string, { expires: 7 })
+        Cookies.set("refresh-token", res.data.session?.refresh_token as string, { expires: 7 })
+        dispatch(updateUser({
+          Name: res.data.user?.user_metadata.name,
+          Class: res.data.user?.user_metadata.class,
+          Email: res.data.user?.email as string,
+          ImageHash: res.data.user?.user_metadata.image.hash,
+          ImageUrl: res.data.user?.user_metadata.image.url
+        }))
+        setLoading(false);
+        navigate("/")
+        return "Login success";
+      },
+      error: (err) => {
+        setLoading(false);
+        return "Invalid login credentials";
+      },
+    });
+  };
+
+  if (auth) return <Navigate to="/" />
 
   return (
     <div className="min-h-screen bg-primary-0 flex">
@@ -45,11 +100,11 @@ const Login = () => {
               type="email"
               error={!!errors.Email}
               {...register("Email", {
-                required: "Please enter your email address!",
+                required: "Email is required",
                 pattern: {
                   value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
-                  message: "Please enter a valid email address!"
-                }
+                  message: "Must be a valid email",
+                },
               })}
             />
             <p
@@ -63,9 +118,10 @@ const Login = () => {
             <Input
               placeholder="Input your password"
               type="password"
+              autoComplete="on"
               error={!!errors.Password}
               {...register("Password", {
-                required: "Please input your password!",
+                required: "Password is required",
               })}
             />
             <p
@@ -82,7 +138,30 @@ const Login = () => {
             </Link>
             <div className="w-full mt-4">
               <Button type="submit" buttonFull>
-                Login
+                {loading ? (
+                  <svg
+                    className="animate-spin h-5 w-5 text-white mx-auto"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                ) : (
+                  "Login"
+                )}
               </Button>
             </div>
             <p className="text-xs text-secondary-300 text-center mt-6">
