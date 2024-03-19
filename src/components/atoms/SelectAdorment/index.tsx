2048c63db540b840288edd62eb16400f9fadfc77
Ivanrizkys <ivnrzkys@gmail.com>
2024-03-19 10:38:27 +0700

    chore: improve accessbility selectAddorment by implement radix ui

--- src/components/atoms/SelectAdorment/index.tsx
+++ src/components/atoms/SelectAdorment/index.tsx
@@ -1,92 +1,61 @@
-// import { OptionsContainer } from "./custom";
-// import { forwardRef, ReactNode, useEffect, useRef, useState } from "react";
-// import closeClickOutside, {
-//   CloseClickOutsideProps,
-// } from "@/src/components/hoc/closeClickOutside";
+import {
+  Content,
+  Item,
+  ItemText,
+  Portal,
+  Root,
+  Trigger,
+  Value,
+  Viewport,
+} from "@radix-ui/react-select";
+import { Dispatch, ReactNode, SetStateAction, forwardRef } from "react";
 
-// interface OptionProps {
-//   value: string;
-//   children: ReactNode;
-// }
+interface SelectItem {
+  value: string;
+  title: string;
+}
 
-// interface SelectAdormentProps extends CloseClickOutsideProps {
-//   placeholder?: string;
-//   defaultText: string;
-//   children: React.ReactNode;
-//   icon: React.ReactNode;
-//   setValue: React.Dispatch<React.SetStateAction<string>>;
-// }
+interface SelectAdormentProps {
+  items: SelectItem[];
+  value: string;
+  icon: ReactNode;
+  placeholder?: string;
+  setValue: Dispatch<SetStateAction<string>>;
+}
 
-// const SelectAdorment = forwardRef<any, SelectAdormentProps>(
-//   (
-//     { placeholder, defaultText, icon, children, setValue, open, setOpen },
-//     ref,
-//   ) => {
-//     const [title, setTitle] = useState<string>("");
+const SelectAdorment = forwardRef<any, SelectAdormentProps>(
+  ({ items, value, icon, placeholder, setValue }) => {
+    return (
+      <Root value={value} onValueChange={setValue}>
+        <Trigger className="flex items-center gap-[12px] rounded-default px-7 py-[14px] border border-solid border-[#F5F5F7] outline-none">
+          {icon}
+          <Value aria-label={value}>
+            <span className="text-xs font-semibold text-secondary-500">
+              {placeholder ? `${placeholder} ` : ""}
+              {items.find((item) => item.value === value)?.title}
+            </span>
+          </Value>
+        </Trigger>
+        <Portal>
+          <Content
+            position="popper"
+            className="w-radix-select-width bg-primary-0 rounded-default z-10"
+          >
+            <Viewport>
+              {items.map((item) => (
+                <Item
+                  value={item.value}
+                  className="text-xs text-secondary-500 font-semibold py-2 px-[10px] hover:bg-[#F5F5F7] cursor-pointer outline-none"
+                >
+                  <ItemText>{item.title}</ItemText>
+                </Item>
+              ))}
+            </Viewport>
+          </Content>
+        </Portal>
+      </Root>
+    );
+  },
+);
 
-//     const optionEl = useRef<HTMLDivElement>(null);
-
-//     useEffect(() => {
-//       const handleClick = (e: Event) => {
-//         const unknownEvent = e as unknown;
-//         const event = unknownEvent as React.MouseEvent<
-//           HTMLDivElement,
-//           MouseEvent
-//         >;
-//         const eventTarget = event.target as HTMLDivElement;
-//         setValue(eventTarget.getAttribute("data-value") ?? "");
-//         setTitle(eventTarget.innerText);
-//       };
-//       const selectItems = optionEl.current?.childNodes;
-//       if (selectItems) {
-//         selectItems.forEach((item) => {
-//           item.addEventListener("click", handleClick);
-//         });
-//       }
-
-//       return () => {
-//         if (selectItems) {
-//           selectItems.forEach((item) => {
-//             item.removeEventListener("click", handleClick);
-//           });
-//         }
-//       };
-//     }, []);
-
-//     return (
-//       <div className="flex flex-col text-secondary-500 text-xs font-medium relative">
-//         <button
-//           ref={ref}
-//           onClick={() => setOpen(!open)}
-//           className="flex items-center justify-between gap-x-[12px] px-7 py-[14px] rounded-default border-[1px] border-solid cursor-pointer border-[#F5F5F7]"
-//         >
-//           {icon}
-//           <p className="text-xs font-semibold text-secondary-500">
-//             {placeholder ? `${placeholder} ` : ""}
-//             {title ? title : defaultText}
-//           </p>
-//         </button>
-//         <OptionsContainer
-//           ref={optionEl}
-//           active={open}
-//           className="absolute top-[55px]"
-//         >
-//           {children}
-//         </OptionsContainer>
-//       </div>
-//     );
-//   },
-// );
-
-// export const Option = ({ value, children }: OptionProps) => {
-//   return (
-//     <div
-//       data-value={value}
-//       className="text-xs text-secondary-500 py-2 px-[10px] hover:bg-[#F5F5F7] cursor-pointer"
-//     >
-//       {children}
-//     </div>
-//   );
-// };
-
-// export default closeClickOutside<SelectAdormentProps>(SelectAdorment);
+export default SelectAdorment;
