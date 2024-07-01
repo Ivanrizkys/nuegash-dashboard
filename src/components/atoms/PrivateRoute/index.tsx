bd49a2e94e239bf19d351ec46669bc609a7cd921
Ivanrizkys <ivnrzkys@gmail.com>
2024-07-01 19:51:08 +0700

    feat: adding fallback loader in private route

--- src/components/atoms/PrivateRoute/index.tsx
+++ src/components/atoms/PrivateRoute/index.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+import PageLoader from "../PageLoader";
 import useAuth from "@/src/hooks/useAuth";
 import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "@/src/components/organisms/Sidebar";
@@ -7,7 +9,11 @@ const PrivateRoute = () => {
   return auth ? (
     <>
       <Sidebar />
-      <Outlet />
+      <div className="xl:ml-[252px] bg-[#FAFAFA] min-h-screen">
+        <Suspense fallback={<PageLoader />}>
+          <Outlet />
+        </Suspense>
+      </div>
     </>
   ) : (
     <Navigate to="/auth/login" />
